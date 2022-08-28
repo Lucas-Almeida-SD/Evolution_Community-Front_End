cdb943ddac34419d72d50121a797072e42fec99f
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-28 14:22:12 -0300

    feat: criado componente que contém formulário de informações da comunidade

---
+++ src/components/CommunityForm.tsx
@@ -0,0 +1,73 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import technologyCommunityImg from '../assets/technology_community.svg';
+import healthCommunityImg from '../assets/health_community.svg';
+import environmentCommunityImg from '../assets/environment_community.svg';
+import businessCommunityImg from '../assets/business_community.svg';
+import { UserInfo } from '../interfaces/User.interface';
+
+type Props = {
+  setFinishedStep: Dispatch<SetStateAction<number>>
+  createUser: UserInfo
+  setCreateUser: Dispatch<SetStateAction<UserInfo>>
+};
+
+function CommunityForm(props: Props) {
+  const { setFinishedStep, createUser, setCreateUser } = props;
+
+  const plans = [
+    { title: 'Tecnologia', id: 'technology-community', src: technologyCommunityImg },
+    { title: 'Saúde', id: 'health-community', src: healthCommunityImg },
+    { title: 'Meio Ambiente', id: 'environment-community', src: environmentCommunityImg },
+    { title: 'Business', id: 'business-community', src: businessCommunityImg },
+  ];
+
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = target;
+    setCreateUser((currentValue) => ({ ...currentValue, [name]: value }));
+  };
+
+  const renderPlanCard = (
+    title: string,
+    srcImg: string,
+    altImg: string,
+    id: string,
+  ) => (
+    <div
+      className={`community-card${(id === createUser.community) ? ' selected' : ''}`}
+      key={id}
+    >
+      <span>{title}</span>
+      <label htmlFor={id}>
+        <img src={srcImg} alt={altImg} />
+        <input
+          id={id}
+          type="radio"
+          value={id}
+          name="community-card"
+          onChange={handleChange}
+        />
+      </label>
+    </div>
+  );
+
+  const handleClickPreviousBtn = () => {
+    setFinishedStep((currentValue) => currentValue - 1);
+  };
+
+  const handleClickNextBtn = () => {
+    setFinishedStep((currentValue) => currentValue + 1);
+  };
+
+  return (
+    <form id="community-form">
+      <h2>Selecione sua comunidade</h2>
+      {plans.map((plan) => renderPlanCard(plan.title, plan.src, plan.title, plan.id))}
+      <div className="change-step">
+        <button type="button" onClick={handleClickPreviousBtn}>Voltar</button>
+        <button type="button" onClick={handleClickNextBtn}>Finalizar</button>
+      </div>
+    </form>
+  );
+}
+
+export default CommunityForm;
