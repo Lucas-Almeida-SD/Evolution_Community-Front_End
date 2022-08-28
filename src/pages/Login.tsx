630595e9cf6fb66cd28e77f404b428aaee7eaa60
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-27 22:55:13 -0300

    feat: adicionado section que aborda uma breve descrição do site

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,9 @@
 import React, { useState, FormEvent, useContext } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
+import { Link } from 'react-router-dom';
 import MyContext from '../context/MyContext';
 import { DataUser } from '../interfaces/User.interface';
+import informationImg from '../assets/information.svg';
 
 function Login() {
   const { isFetching, setIsFetching, setUser } = useContext(MyContext);
@@ -38,49 +40,57 @@ function Login() {
   };
 
   return (
-    <main>
-      <form onSubmit={handleSubmit}>
-        <h2>
-          <span>Evolution </span>
-          <span>Community</span>
-        </h2>
-        <input
-          type="text"
-          id="email"
-          value={email}
-          placeholder="Email"
-          onChange={({ target }) => setEmail(target.value)}
-        />
-        <input
-          type="password"
-          id="password"
-          value={password}
-          placeholder="Senha"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-        <button
-          type="submit"
-          disabled={isFetching || !email || !password}
-        >
-          Entrar
-        </button>
-        <div>
-          <button
-            type="button"
-            disabled={isFetching}
-          >
-            Esqueci minha senha
-          </button>
-          <button
-            type="button"
-            disabled={isFetching}
-          >
-            Inscreva-se
-          </button>
-        </div>
-      </form>
+    <>
+      <main>
+        <section>
+          <p>
+            Faça parte de uma de nossas comunidades e receba informações de
+            qualidade. Concorra também a prêmios imperdíveis semanalmente!
+          </p>
+          <img src={informationImg} alt="Information" />
+        </section>
+        <section>
+          <form onSubmit={handleSubmit}>
+            <h2>
+              <span>Evolution </span>
+              <span>Community</span>
+            </h2>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              placeholder="Email"
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              placeholder="Senha"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              type="submit"
+              disabled={isFetching || !email || !password}
+            >
+              Entrar
+            </button>
+            <div>
+              <span>
+                Esqueci minha senha
+              </span>
+              <Link
+                to="/registration"
+                type="button"
+              >
+                Inscreva-se
+              </Link>
+            </div>
+          </form>
+        </section>
+      </main>
       <Toaster />
-    </main>
+    </>
   );
 }
 
