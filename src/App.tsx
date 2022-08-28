dd1085d20a7df588b29b29fbea2e73f2b47a579c
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-28 11:33:26 -0300

    feat: criada rotas para as pages de Login e de registro

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
+import './styles/global.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/registration" component={Registration} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
