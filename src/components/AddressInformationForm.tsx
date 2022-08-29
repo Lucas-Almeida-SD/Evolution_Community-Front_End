1463a5b8bcea4795d9d1baa223b9adb8870ab48a
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-28 22:41:57 -0300

    feat: adidionada as validações dos campos dos forms

--- src/components/AddressInformationForm.tsx
+++ src/components/AddressInformationForm.tsx
@@ -1,14 +1,24 @@
 import React, { Dispatch, SetStateAction } from 'react';
+import {
+  validateAddress,
+  validateCEP,
+  validateCity,
+  validateDistrict,
+  validateHouseNumber,
+  validatePublicPlace,
+} from '../helpers/userValidations';
 import { UserInfo } from '../interfaces/User.interface';
 import '../styles/AddressInformationForm.scss';
+import Input from './Input';
 
 type Props = {
   setFinishedStep: Dispatch<SetStateAction<number>>
+  createUser: UserInfo
   setCreateUser: Dispatch<SetStateAction<UserInfo>>
 };
 
 function AddressInformationForm(props: Props) {
-  const { setFinishedStep, setCreateUser } = props;
+  const { setFinishedStep, createUser, setCreateUser } = props;
 
   const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = target;
@@ -23,40 +33,104 @@ function AddressInformationForm(props: Props) {
     setFinishedStep((currentValue) => currentValue + 1);
   };
 
+  const fieldsIsValid = () => (
+    validateAddress(createUser)
+    && validateCEP(createUser)
+    && validateCity(createUser)
+    && validateDistrict(createUser)
+    && validateHouseNumber(createUser)
+    && validatePublicPlace(createUser)
+  );
+
   return (
     <form id="address-information-form">
       <h2>Informações de Endereço</h2>
-      <label htmlFor="publicPlace">
-        <span>Logradouro</span>
-        <input id="publicPlace" type="text" name="publicPlace" onChange={handleChange} />
-      </label>
-      <label htmlFor="address">
-        <span>Endereço</span>
-        <input id="address" type="text" name="address" onChange={handleChange} />
-      </label>
-      <label htmlFor="houseNumber">
-        <span>Número</span>
-        <input id="houseNumber" type="number" name="houseNumber" onChange={handleChange} />
-      </label>
-      <label htmlFor="complement">
-        <span>Complemento</span>
-        <input id="complement" type="text" name="complement" onChange={handleChange} />
-      </label>
-      <label htmlFor="district">
-        <span>Bairro</span>
-        <input id="district" type="text" name="district" onChange={handleChange} />
-      </label>
-      <label htmlFor="city">
-        <span>Cidade</span>
-        <input id="city" type="text" name="city" onChange={handleChange} />
-      </label>
-      <label htmlFor="cep">
-        <span>CEP</span>
-        <input id="cep" type="text" name="CEP" onChange={handleChange} />
-      </label>
+      <Input
+        htmlFor="publicPlace"
+        spanContent="Logradouro"
+        id="publicPlace"
+        type="text"
+        name="publicPlace"
+        value={createUser.publicPlace}
+        isValid={validatePublicPlace(createUser)}
+        onChange={handleChange}
+      />
+      <Input
+        htmlFor="address"
+        spanContent="Endereço"
+        id="address"
+        type="text"
+        name="address"
+        value={createUser.address}
+        isValid={validateAddress(createUser)}
+        onChange={handleChange}
+      />
+      <Input
+        htmlFor="houseNumber"
+        spanContent="Número"
+        id="houseNumber"
+        type="number"
+        name="houseNumber"
+        value={createUser.houseNumber}
+        isValid={validateHouseNumber(createUser)}
+        onChange={handleChange}
+      />
+      <Input
+        htmlFor="complement"
+        spanContent="Complemento"
+        id="complement"
+        type="text"
+        name="complement"
+        value={createUser.complement}
+        isValid
+        onChange={handleChange}
+      />
+      <Input
+        htmlFor="district"
+        spanContent="Bairro"
+        id="district"
+        type="text"
+        name="district"
+        value={createUser.district}
+        isValid={validateDistrict(createUser)}
+        onChange={handleChange}
+      />
+      <Input
+        htmlFor="city"
+        spanContent="Cidade"
+        id="city"
+        type="text"
+        name="city"
+        value={createUser.city}
+        isValid={validateCity(createUser)}
+        onChange={handleChange}
+      />
+      <Input
+        htmlFor="cep"
+        spanContent="CEP"
+        id="cep"
+        type="text"
+        name="CEP"
+        value={createUser.CEP}
+        isValid={validateCEP(createUser)}
+        onChange={handleChange}
+      />
       <div className="change-step">
-        <button type="button" className="back" onClick={handleClickPreviousBtn}>Voltar</button>
-        <button type="button" className="next" onClick={handleClickNextBtn}>Próximo</button>
+        <button
+          type="button"
+          className="back"
+          onClick={handleClickPreviousBtn}
+        >
+          Voltar
+        </button>
+        <button
+          type="button"
+          className="next"
+          disabled={!fieldsIsValid()}
+          onClick={handleClickNextBtn}
+        >
+          Próximo
+        </button>
       </div>
     </form>
   );
--- src/components/PersonalInformationForm.tsx
+++ src/components/PersonalInformationForm.tsx
@@ -1,14 +1,25 @@
 import React, { Dispatch, SetStateAction } from 'react';
+import {
+  validateBirthDate,
+  validateCPF,
+  validateEmail,
+  validateFullname,
+  validatePassword,
+  validatePhone,
+  validateRG,
+} from '../helpers/userValidations';
 import { UserInfo } from '../interfaces/User.interface';
 import '../styles/PersonalInformationForm.scss';
+import Input from './Input';
 
 type Props = {
   setFinishedStep: Dispatch<SetStateAction<number>>
+  createUser: UserInfo
   setCreateUser: Dispatch<SetStateAction<UserInfo>>
 };
 
 function PersonalInformationForm(props: Props) {
-  const { setFinishedStep, setCreateUser } = props;
+  const { setFinishedStep, createUser, setCreateUser } = props;
 
   const handleClick = () => {
     setFinishedStep((currentValue) => currentValue + 1);
@@ -19,38 +30,90 @@ function PersonalInformationForm(props: Props) {
     setCreateUser((currentValue) => ({ ...currentValue, [name]: value }));
   };
 
+  const fieldsIsValid = () => (
+    validateBirthDate(createUser)
+      && validateCPF(createUser)
+      && validateEmail(createUser)
+      && validateFullname(createUser)
+      && validatePassword(createUser)
+      && validatePhone(createUser)
+      && validateRG(createUser)
+  );
+
   return (
     <form id="personal-information-form">
       <h2>Informações Pessoais</h2>
-      <label htmlFor="fullname">
-        <span>Nome completo</span>
-        <input id="fullname" type="text" name="fullname" onChange={handleChange} />
-      </label>
-      <label htmlFor="birthDate">
-        <span>Data de Nascimento</span>
-        <input id="birthDate" type="date" name="birthDate" onChange={handleChange} />
-      </label>
-      <label htmlFor="rg">
-        <span>RG</span>
-        <input id="rg" type="text" name="RG" onChange={handleChange} />
-      </label>
-      <label htmlFor="cpf">
-        <span>CPF</span>
-        <input id="cpf" type="text" name="CPF" onChange={handleChange} />
-      </label>
-      <label htmlFor="phone">
-        <span>Telefone</span>
-        <input id="phone" type="text" name="phone" onChange={handleChange} />
-      </label>
-      <label htmlFor="email">
-        <span>Email</span>
-        <input id="email" type="text" name="email" onChange={handleChange} />
-      </label>
-      <label htmlFor="password">
-        <span>Senha</span>
-        <input id="password" type="password" name="password" onChange={handleChange} />
-      </label>
-      <button type="button" onClick={handleClick}>Próximo</button>
+      <Input
+        htmlFor="fullname"
+        spanContent="Nome completo"
+        id="fullname"
+        type="text"
+        name="fullname"
+        value={createUser.fullname}
+        isValid={validateFullname(createUser)}
+        onChange={handleChange}
+      />
+      <Input
+        htmlFor="birthDate"
+        spanContent="Data de Nascimento"
+        id="birthDate"
+        type="date"
+        name="birthDate"
+        value={createUser.birthDate}
+        isValid={validateBirthDate(createUser)}
+        onChange={handleChange}
+      />
+      <Input
+        htmlFor="rg"
+        spanContent="RG"
+        id="rg"
+        type="text"
+        name="RG"
+        value={createUser.RG}
+        isValid={validateRG(createUser)}
+        onChange={handleChange}
+      />
+      <Input
+        htmlFor="cpf"
+        spanContent="CPF"
+        id="cpf"
+        type="text"
+        name="CPF"
+        value={createUser.CPF}
+        isValid={validateCPF(createUser)}
+        onChange={handleChange}
+      />
+      <Input
+        htmlFor="phone"
+        spanContent="Telefone"
+        id="phone"
+        type="text"
+        name="phone"
+        value={createUser.phone}
+        isValid={validatePhone(createUser)}
+        onChange={handleChange}
+      />
+      <Input
+        htmlFor="email"
+        spanContent="Email"
+        id="email"
+        type="text"
+        name="email"
+        value={createUser.email}
+        isValid={validateEmail(createUser)}
+        onChange={handleChange}
+      />
+      <Input
+        htmlFor="password"
+        spanContent="Senha"
+        id="password"
+        type="password"
+        name="password"
+        value={createUser.password}
+        isValid={validatePassword(createUser)}
+        onChange={handleChange}
+      />
+      <button type="button" disabled={!fieldsIsValid()} onClick={handleClick}>Próximo</button>
     </form>
   );
 }
