69fde1df73b37e72939bc15e75a71e21edf256d5
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-29 02:38:01 -0300

    feat: criada função que realiza a requisição de cadastro do user

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import PersonalInformationForm from '../components/PersonalInformationForm';
 import AddressInformationForm from '../components/AddressInformationForm';
@@ -7,10 +7,14 @@ import checkImg from '../assets/check.png';
 import { UserInfo } from '../interfaces/User.interface';
 import '../styles/Registration.scss';
 import TerminatedUser from '../components/TerminatedUser';
+import MyContext from '../context/MyContext';
 
 function Registration() {
+  const { isFetching, setIsFetching } = useContext(MyContext);
   const [finishedStep, setFinishedStep] = useState<number>(0);
   const { location: { pathname } } = useHistory();
+
+  const URL = 'https://evolution-community.herokuapp.com/users';
 
   const message = (pathname.includes('registration'))
     ? 'Usuário cadastrado com sucesso!'
@@ -33,6 +37,25 @@ function Registration() {
     community: '',
     publicPlace: '',
   });
+
+  const request = async (): Promise<{ message: string, error: boolean }> => {
+    const newCreateUser = {
+      ...createUser,
+      birthDate: createUser.birthDate.split('-').reverse().join('/'),
+    };
+
+    const result = await fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(newCreateUser),
+    });
+
+    const json = await result.json();
+
+    return json as { message: string, error: boolean };
+  };
 
   const renderSteps = (): JSX.Element[] => {
     const stepsNumber = 3;
@@ -59,31 +82,34 @@ function Registration() {
         </div>
         <div className="registration-forms">
           {(finishedStep === 0) && (
-            <PersonalInformationForm
-              setFinishedStep={setFinishedStep}
-              createUser={createUser}
-              setCreateUser={setCreateUser}
-            />
+          <PersonalInformationForm
+            setFinishedStep={setFinishedStep}
+            createUser={createUser}
+            setCreateUser={setCreateUser}
+          />
           )}
           {(finishedStep === 1) && (
-            <AddressInformationForm
-              setFinishedStep={setFinishedStep}
-              createUser={createUser}
-              setCreateUser={setCreateUser}
-            />
+          <AddressInformationForm
+            setFinishedStep={setFinishedStep}
+            createUser={createUser}
+            setCreateUser={setCreateUser}
+          />
           )}
           {(finishedStep === 2) && (
-            <CommunityForm
-              setFinishedStep={setFinishedStep}
-              createUser={createUser}
-              setCreateUser={setCreateUser}
-            />
+          <CommunityForm
+            setFinishedStep={setFinishedStep}
+            createUser={createUser}
+            setCreateUser={setCreateUser}
+            request={request}
+            isFetching={isFetching}
+            setIsFetching={setIsFetching}
+          />
           )}
           {(finishedStep > 2) && (
-            <TerminatedUser
-              message={message}
-              goTo={(pathname.includes('registration') ? '/' : '/dashboard')}
-            />
+          <TerminatedUser
+            message={message}
+            goTo={(pathname.includes('registration') ? '/' : '/dashboard')}
+          />
           )}
         </div>
       </section>
