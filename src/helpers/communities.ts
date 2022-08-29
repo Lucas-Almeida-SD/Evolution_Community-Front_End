7475aeb6b3e9f528f844a9f50f13c1067cc4398a
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-29 16:54:03 -0300

    feat: criado array com informações das comunidades

---
+++ src/helpers/communities.ts
@@ -0,0 +1,31 @@
+import technologyCommunityImg from '../assets/technology_community.svg';
+import healthCommunityImg from '../assets/health_community.svg';
+import environmentCommunityImg from '../assets/environment_community.svg';
+import businessCommunityImg from '../assets/business_community.svg';
+
+export default [
+  {
+    title: 'Tecnologia',
+    value: 'technology',
+    id: 'technology-community',
+    src: technologyCommunityImg,
+  },
+  {
+    title: 'Saúde',
+    value: 'health',
+    id: 'health-community',
+    src: healthCommunityImg,
+  },
+  {
+    title: 'Meio Ambiente',
+    value: 'environment',
+    id: 'environment-community',
+    src: environmentCommunityImg,
+  },
+  {
+    title: 'Business',
+    value: 'business',
+    id: 'business-community',
+    src: businessCommunityImg,
+  },
+];
