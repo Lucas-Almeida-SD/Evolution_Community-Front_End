e34abae3c8574654ea372d9735d75a4e0ab6d684
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-30 20:08:44 -0300

    feat: adicionado componente Toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Registration from './pages/Registration';
@@ -7,14 +8,17 @@ import './styles/global.scss';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/registration" component={Registration} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/edit-profile" component={Registration} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/registration" component={Registration} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/edit-profile" component={Registration} />
+        </Switch>
+      </BrowserRouter>
+      <Toaster />
+    </>
   );
 }
 
