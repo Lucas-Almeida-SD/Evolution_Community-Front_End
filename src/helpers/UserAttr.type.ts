2c612fc37e684be9cbd6a7f5401fb7d978d74df2
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-31 16:42:51 -0300

    feat: criado type UserAttr

---
+++ src/helpers/UserAttr.type.ts
@@ -0,0 +1,5 @@
+type UserAttr = 'fullname' | 'birthDate' | 'RG' | 'CPF' | 'phone' | 'email'
+| 'password' | 'publicPlace' | 'address' | 'houseNumber' | 'complement' | 'district'
+| 'city' | 'CEP';
+
+export default UserAttr;
