77172bdedfe47f580208aead2c1c844a00643c9f
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-31 16:43:48 -0300

    feat: criado objeto com as mensagens de erro das validações do inputs

---
+++ src/helpers/formInputErrorMessage.ts
@@ -0,0 +1,16 @@
+export default {
+  fullname: 'Deve conter apenas letras e possuir comprimento mínimo de 3 caracteres.',
+  birthDate: 'Deve conter um data válida.',
+  RG: 'Deve conter apenas caracteres numéricos.',
+  CPF: 'Deve conter apenas caracteres numéricos e possuir comprimento igual a 11 caracteres.',
+  phone: 'Deve conter apenas caracteres numéricos.',
+  email: 'Deve ser um email no formato "exemplo@exemplo.com".',
+  password: 'Deve conter apenas caracteres numéricos e comprimento mínimo de 8 caracteres.',
+  publicPlace: 'Deve conter apenas letras.',
+  address: 'Deve conter apenas letras e/ou caracteres numéricos.',
+  houseNumber: 'Deve conter apenas caracteres numéricos.',
+  complement: '(Opcional) - Deve conter apenas letras e/ou caracteres numéricos.',
+  district: 'Deve conter apenas letras e/ou caracteres numéricos.',
+  city: 'Deve conter apenas letras e/ou caracteres numéricos.',
+  CEP: 'Deve conter apenas caracteres numéricos e comprimento igual a 8 caracteres.',
+};
